870f2c6d8d341ba5f62610a1d612a8f3fe2162f0
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-06-10 14:55:49 -0700

    Generalize build script

--- build_dev.sh
+++ build_dev.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 set -e -x
 
+build_image=outstand/shipitron:dev
+dockerfile=Dockerfile
+bundler_data_dir=tmp
+
 bundler_data_container=''
 tar_container=''
 
 function cleanup {
   if [ -n "$bundler_data_container" ]; then
     docker stop bundler-data
-    docker rm -f bundler-data
+    docker rm -fv bundler-data
   fi
 
   if [ -n "$tar_container" ]; then
-    docker rm -f ${tar_container}
+    docker rm -fv ${tar_container}
   fi
 
-  rm -f tmp/cidfile
+  rm -f ${bundler_data_dir}/cidfile
 }
 
 trap cleanup EXIT
 
 build_args=''
-mkdir -p tmp
+mkdir -p ${bundler_data_dir}
 
-if [ -f $(pwd)/tmp/bundler-data.tar.gz ]; then
-  docker run --name bundler-data -v $(pwd)/tmp/bundler-data.tar.gz:/usr/share/nginx/html/bundler-data.tar.gz:ro -d nginx:stable-alpine
+if [ -f $(pwd)/${bundler_data_dir}/bundler-data.tar.gz ]; then
+  docker run --name bundler-data -v $(pwd)/${bundler_data_dir}/bundler-data.tar.gz:/usr/share/nginx/html/bundler-data.tar.gz:ro -d nginx:stable-alpine
   bundler_data_container=bundler-data
   build_args="--build-arg bundler_data_host=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' bundler-data)"
 fi
 
-docker build -t outstand/shipitron:dev ${build_args} .
+docker build -t ${build_image} -f ${dockerfile} ${build_args} .
 
-docker run -t --cidfile=tmp/cidfile -w /usr/local/bundle outstand/shipitron:dev tar -zcf /tmp/bundler-data.tar.gz .
-tar_container=$(cat tmp/cidfile)
-docker cp ${tar_container}:/tmp/bundler-data.tar.gz tmp/bundler-data.tar.gz
+docker run -t --cidfile=${bundler_data_dir}/cidfile -w /usr/local/bundle ${build_image} tar -zcf /tmp/bundler-data.tar.gz .
+tar_container=$(cat ${bundler_data_dir}/cidfile)
+docker cp ${tar_container}:/tmp/bundler-data.tar.gz ${bundler_data_dir}/bundler-data.tar.gz
