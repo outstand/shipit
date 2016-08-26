379e5454c2a140310d37183c60fafe0d3e689c0a
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-08-26 16:38:30 -0700

    show errors on startup

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -7,7 +7,7 @@ else
   BINARY=shipitron
 fi
 
-if ${BINARY} help "$1" 2>&1 | grep -q "shipitron $1"; then
+if ${BINARY} help "$1" | grep -q "shipitron $1"; then
   set -- gosu shipitron ${BINARY} "$@"
 
   if [ -n "$FOG_LOCAL" ]; then
