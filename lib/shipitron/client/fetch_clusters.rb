6e0f15c34f735f4160f564083e9e3eec88b7abc3
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-01-24 13:32:05 -0800

    Add specs and fix FetchClusters

--- lib/shipitron/client/fetch_clusters.rb
+++ lib/shipitron/client/fetch_clusters.rb
@@ -9,22 +9,33 @@ module Shipitron
       required :cluster_discovery
 
       def call
+        resources = dns_resources.sort! do |a,b|
+          (a.priority <=> b.priority).yield_self do |prio|
+            if prio == 0
+              b.weight <=> a.weight
+            else
+              prio
+            end
+          end
+        end
+
+        context.clusters = resources.map do |r|
+          Smash.new(
+            name: r.target[0].to_s,
+            region: r.target[1].to_s
+          )
+        end
+
+        Logger.debug "Clusters: #{context.clusters.inspect}"
+      end
+
+      private
+      def dns_resources
         Resolv::DNS.open do |dns|
-          resources = dns.getresources(
+          dns.getresources(
             context.cluster_discovery,
             Resolv::DNS::Resource::IN::SRV
-          ).sort_by! do |a|
-            [a.priority, a.weight]
-          end.reverse!
-
-          context.clusters = resources.map do |r|
-            Smash.new(
-              name: r.target[0].to_s,
-              region: r.target[1].to_s
-            )
-          end
-
-          Logger.debug "Clusters: #{context.clusters.inspect}"
+          )
         end
       end
     end
