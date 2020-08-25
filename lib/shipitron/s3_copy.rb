e7fab525f853635cfa383d4598459bc410b91dc9
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-08-25 14:50:42 -0700

    Try hitting the metadata endpoint

--- lib/shipitron/s3_copy.rb
+++ lib/shipitron/s3_copy.rb
@@ -16,6 +16,7 @@ module Shipitron
         end
       else
         Logger.info "S3 Copy from #{source} to #{destination}"
+        Logger.info `curl ${ECS_CONTAINER_METADATA_URI_V4}/task`
         Logger.info `docker run --rm -t -v shipitron-home:/home/shipitron -e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI amazon/aws-cli:latest --region #{region} s3 cp #{source} #{destination} --quiet --only-show-errors`
         if $? != 0
           fail_with_error!(message: 'Failed to transfer to/from s3.')
