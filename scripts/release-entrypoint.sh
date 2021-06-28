77e125f6bd7693b0570c872d9fd89f909900e0c5
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-06-28 15:39:14 -0700

    Add dedicated `staging` setup

---
+++ scripts/release-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -euo pipefail
+
+if [ -n "$USE_BUNDLE_EXEC" ]; then
+  BINARY="bundle exec shipitron"
+else
+  BINARY=shipitron
+fi
+
+${BINARY} help "$1"
+
+if ls /usr/local/bundle/bin | grep -q "\b$1\b"; then
+  set -- su-exec shipitron bundle exec "$@"
+
+elif ${BINARY} help "$1" 2>&1 | grep -q "shipitron $1"; then
+  set -- su-exec shipitron ${BINARY} "$@"
+fi
+
+exec "$@"
